0c4a3a2f7100986500fc0209cb1dd5c4cd7631b4
JoGeorges <141683247+JoGeorges@users.noreply.github.com>
2023-12-02 14:39:44 -0500

    Add files via upload

---
+++ battle2dec.py
@@ -0,0 +1,9 @@
+print("\t")
+print("\tBonjou, bonswa tout moun,")
+print("\tMwen espere ke nou anfòm.")
+print("\tSa se battle 2 desanm lan.")
+user = input("\t- Rantre non w: ")
+final = ' '.join(user.capitalize() for i in user.split())
+print(final)
+
+       
