c92ff10464a2551d4407aa04ba9c7ebef9b3c75d
JoGeorges <141683247+JoGeorges@users.noreply.github.com>
2023-12-09 18:13:05 -0500

    Update battle_9desanm.py

--- battle_9desanm.py
+++ battle_9desanm.py
@@ -4,13 +4,11 @@ print("\tPwogram sila a, se battle dat 3 desanm lan.")
 def minter(entèval):
     intert = sorted(entèval, key=lambda x: x[0])
     all = [intert[0]]
-    
     for entèv in intert:
         if entèv[0] <= all[-1][1]:
             all[-1][1] = max(all[-1][1], entèv[1])
         else:
             all.append(entèv)
-    
     return all
 entèval = [[1,4], [3,6]]
 rezilta = minter(entèval)
